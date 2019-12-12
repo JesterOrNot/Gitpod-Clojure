06b20d7d1f5318a4e98bc0662dce6aa47ce12cbe
Sean Hellum <seanhellum45@gmail.com>
2019-12-12 14:31:30 +0000

    update

--- Test.clj
+++ Test.clj
@@ -1,8 +1,10 @@
 (defn greeter
   []
-  (println "Whats your name?: ")
-  (let [my_name (read-line)]
-    (if (= my_name "Sean")
-        (println "Thats my name get your own name!")
-        (println (str "Well Hello " my_name "!")))))
+  (do
+    (print "Whats your name?: ")
+    (flush)
+    (let [my_name (read-line)]
+        (if (= my_name "Sean")
+            (println "Thats my name get your own name!")
+            (println (str "Well Hello " my_name "!"))))))
 (greeter)
